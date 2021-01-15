3f662f88cddd39009973fab12d35a1ffd4282c9c
Saliha Beddek <beddeksaliha@gmail.com>
2021-01-15 17:16:45 +0100

    update Admin

--- ProjetNestiPart2/src/Admin.java
+++ ProjetNestiPart2/src/Admin.java
@@ -1,24 +1,41 @@
+import java.sql.Date;
+
+
+
 
 public class Admin {
 private int id;
 private String firstName;
 private String lastName;
-private String login;
+private String userName;
 private String password;
 private String adress;
 private String state;
-private boolean isSuperAdmin;
-//private date creatDate;
-//private date updateDate;
+private boolean superAdmin;
+Date creationDate;
+Date updateDate;
+
+public Admin(int id, String firstName, String lastName, String userName, String password, String adress, String state, boolean superAdmin, Date creationDate, Date updateDate) {
+	this.id= id;
+	this.firstName= firstName;
+	this.lastName= lastName;
+	this.userName=userName;
+	this.password=password;
+	this.state=state;
+	this.creationDate=creationDate;
+	this.updateDate=updateDate;
+	this.superAdmin= superAdmin;
+	
+}
 
 public int getId() {
 	return id;
 }
 public boolean isSuperAdmin() {
-	return isSuperAdmin;
+	return superAdmin;
 }
 public void setSuperAdmin(boolean isSuperAdmin) {
-	this.isSuperAdmin = isSuperAdmin;
+	this.superAdmin = isSuperAdmin;
 }
 public void setId(int id) {
 	this.id = id;
@@ -35,11 +52,11 @@ public String getLastName() {
 public void setLastName(String lastName) {
 	this.lastName = lastName;
 }
-public String getLogin() {
-	return login;
+public String getUserName() {
+	return userName;
 }
-public void setLogin(String login) {
-	this.login = login;
+public void setUserName(String userName) {
+	this.userName = userName;
 }
 public String getPassword() {
 	return password;
@@ -59,10 +76,27 @@ public String getState() {
 public void setState(String state) {
 	this.state = state;
 }
+
+public Date getCreationDate() {
+	return creationDate;
+}
+
+public void setCreationDate(Date creationDate) {
+	this.creationDate = creationDate;
+}
+
+public Date getUpdateDate() {
+	return updateDate;
+}
+
+public void setUpdateDate(Date updateDate) {
+	this.updateDate = updateDate;
+}
+
 @Override
 public String toString() {
-	return "Admin [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", login=" + login
-			+ ", password=" + password + ", adress=" + adress + ", state=" + state + ", isSuperAdmin=" + isSuperAdmin
+	return "Admin [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", userName=" + userName
+			+ ", password=" + password + ", adress=" + adress + ", state=" + state + ", isSuperAdmin=" + superAdmin
 			+ "]";
 }
 
--- ProjetNestiPart2/src/entities/Admin.java
+++ ProjetNestiPart2/src/entities/Admin.java
@@ -1,5 +1,122 @@
 package entities;
 
+import java.sql.Date;
+
+
+
+
+public class Admin {
+private int id;
+private String firstName;
+private String lastName;
+private String userName;
+private String password;
+private String adress;
+private String state;
+private boolean superAdmin;
+Date creationDate;
+Date updateDate;
+
+
+
+public Admin(int id, String firstName, String lastName, String userName, String password, String adress, String state, boolean superAdmin, Date creationDate, Date updateDate) {
+	this.id= id;
+	this.firstName= firstName;
+	this.lastName= lastName;
+	this.userName=userName;
+	this.password=password;
+	this.state=state;
+	this.creationDate=creationDate;
+	this.updateDate=updateDate;
+	this.superAdmin= superAdmin;
+	
+	
+}
+
+public int getId() {
+	return id;
+}
+public boolean isSuperAdmin() {
+	return superAdmin;
+}
+public void setSuperAdmin(boolean isSuperAdmin) {
+	this.superAdmin = isSuperAdmin;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getFirstName() {
+	return firstName;
+}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+public String getLastName() {
+	return lastName;
+}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+public String getUserName() {
+	return userName;
+}
+public void setUserName(String userName) {
+	this.userName = userName;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getAdress() {
+	return adress;
+}
+public void setAdress(String adress) {
+	this.adress = adress;
+}
+public String getState() {
+	return state;
+}
+public void setState(String state) {
+	this.state = state;
+}
+
+public Date getCreationDate() {
+	return creationDate;
+}
+
+public void setCreationDate(Date creationDate) {
+	this.creationDate = creationDate;
+}
+
+public Date getUpdateDate() {
+	return updateDate;
+}
+
+public void setUpdateDate(Date updateDate) {
+	this.updateDate = updateDate;
+}
+
+@Override
+public String toString() {
+	return "Admin [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", userName=" + userName
+			+ ", password=" + password + ", adress=" + adress + ", state=" + state + ", isSuperAdmin=" + superAdmin
+			+ "]";
+}
+
+
+
+	
+	
+}
+
+
+
+
+
+/*package entities;
+
 import java.util.Date;
 
 public class Admin {
@@ -88,4 +205,4 @@ public class Admin {
 		this.superAdmin = superAdmin;
 	}
 	
-}
+}*/
