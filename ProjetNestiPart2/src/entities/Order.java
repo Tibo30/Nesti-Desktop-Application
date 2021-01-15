5d0c3ffad229d9be75e6d9211b054b15b0470f7a
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-01-15 17:10:12 +0100

    add queries and update entities classes

--- ProjetNestiPart2/src/entities/Order.java
+++ ProjetNestiPart2/src/entities/Order.java
@@ -1,10 +1,54 @@
 package entities;
 
+import java.util.Date;
+
 public class Order {
+	String state;
+	Date validationDate;
+	Date deliveryDate;
+	Article article;
+
+	public Order(String state, Date validationDate, Date deliveryDate, Article article) {
+		this.state=state;
+		this.validationDate=validationDate;
+		this.deliveryDate=deliveryDate;
+		this.article= article;
+
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public Date getValidationDate() {
+		return validationDate;
+	}
+
+	public void setValidationDate(Date validationDate) {
+		this.validationDate = validationDate;
+	}
+
+	public Date getDeliveryDate() {
+		return deliveryDate;
+	}
+
+	public void setDeliveryDate(Date deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+
+	public Article getArticle() {
+		return article;
+	}
+
+	public void setArticle(Article article) {
+		this.article = article;
+	}
 	
 	
 	
-public Order() {
 	
-}
 }
