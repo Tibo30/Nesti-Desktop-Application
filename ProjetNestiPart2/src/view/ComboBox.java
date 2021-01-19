efe856f0fb44a1ff21bef0433fdfbf526863638b
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-01-19 15:39:56 +0100

    add change listener to tabbed pane

--- ProjetNestiPart2/src/view/ComboBox.java
+++ ProjetNestiPart2/src/view/ComboBox.java
@@ -10,14 +10,7 @@ public class ComboBox extends JComboBox {
 
 	public ComboBox(String name, int x, int y, int L, int l) throws Exception {
 		this.setBounds(x, y, L, l);
-		
-	if(name.equals("listSupp")) {
-		// get the list of the suppliers
-		for (int i=0;i<QuerySupplier.querySuppl.listAllSupplier().size();i++) {
-			this.addItem(QuerySupplier.querySuppl.listAllSupplier().get(i));
-		}
-		
-	}
+
 		
 		
 	}
