dabfa8dc5a9a9116aafa3e8437fbf9c9aece2cdc
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-01-24 10:56:22 +0100

    update with scrollbar

--- ProjetNestiPart2/src/view/ScrollPane.java
+++ ProjetNestiPart2/src/view/ScrollPane.java
@@ -1,6 +1,7 @@
 package view;
 
 import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 import model.QuerySupplier;
 
@@ -10,7 +11,7 @@ public class ScrollPane extends JScrollPane{
 	public ScrollPane(int x, int y, int L, int l) {
 	
 		this.setBounds(x, y, L, l);		
-		
+		this.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		
 	}
 	
