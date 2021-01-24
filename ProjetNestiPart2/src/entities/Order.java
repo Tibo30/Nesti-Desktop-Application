a18ea9f3c094467642464d6bb79f4abf1a94f810
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-01-24 10:54:00 +0100

    update supplier and order

--- ProjetNestiPart2/src/entities/Order.java
+++ ProjetNestiPart2/src/entities/Order.java
@@ -3,18 +3,35 @@ package entities;
 import java.util.Date;
 
 public class Order {
+	int id;
 	String state;
 	Date validationDate;
 	Date deliveryDate;
-	Article article;
-
-	public Order(String state, Date validationDate, Date deliveryDate, Article article) {
+	Supplier supplier;
+	int idAdmin;
+	
+	public Order(int id, Date validationDate, Date deliveryDate, String state, Supplier supplier, int idAdmin) {
+		this.id=id;
 		this.state=state;
 		this.validationDate=validationDate;
 		this.deliveryDate=deliveryDate;
-		this.article= article;
-
+		this.supplier=supplier;
+		this.idAdmin=idAdmin;
 	}
+	
+	@Override
+	public String toString() {
+		int number;
+		int idLength = String.valueOf(id).length();
+		String orderNumber="";
+		number = 10-idLength;
+		for (int i=0; i<number;i++) {
+			orderNumber+=0;
+		}
+		orderNumber+=String.valueOf(id);
+		return  "Order number : " + orderNumber ;
+	}
+
 
 	public String getState() {
 		return state;
@@ -40,13 +57,37 @@ public class Order {
 		this.deliveryDate = deliveryDate;
 	}
 
-	public Article getArticle() {
-		return article;
+
+	public int getId() {
+		return id;
 	}
 
-	public void setArticle(Article article) {
-		this.article = article;
+
+	public void setId(int id) {
+		this.id = id;
 	}
+
+
+	public Supplier getSupplier() {
+		return supplier;
+	}
+
+
+	public void setSupplier(Supplier supplier) {
+		this.supplier = supplier;
+	}
+
+
+	public int getIdAdmin() {
+		return idAdmin;
+	}
+
+
+	public void setIdAdmin(int idAdmin) {
+		this.idAdmin = idAdmin;
+	}
+	
+	
 	
 	
 	
