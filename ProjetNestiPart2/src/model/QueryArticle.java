c084a57148aa5f30e3d54bcd509169ffeb6f723a
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-01-31 11:15:34 +0100

    add query article

--- ProjetNestiPart2/src/model/QueryArticle.java
+++ ProjetNestiPart2/src/model/QueryArticle.java
@@ -2,16 +2,24 @@ package model;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import entities.Article;
 import entities.Packaging;
 import entities.Product;
 import entities.Supplier;
+import entities.SupplierSell;
 
 public class QueryArticle extends MyConnection {
 	private QueryProduct queryProduct = new QueryProduct();
 
+	/**
+	 * List all articles from the database
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
 	public ArrayList<Article> listAllArticle() throws Exception {
 		ArrayList<Article> listArticle = new ArrayList<Article>();
 		Article art = null;
@@ -28,7 +36,7 @@ public class QueryArticle extends MyConnection {
 				Product product = queryProduct.createProductInfo(rs.getString("product_name"));
 				art = new Article(rs.getInt("id_article"), rs.getDouble("article_quantity"),
 						rs.getInt("article_quantity_real_stock"), rs.getString("article_state"),
-						rs.getDate("article_creation_date"), rs.getDate("article_update_date"), packaging,product,
+						rs.getDate("article_creation_date"), rs.getDate("article_update_date"), packaging, product,
 						rs.getInt("id_admin"));
 				listArticle.add(art);
 			}
@@ -38,7 +46,49 @@ public class QueryArticle extends MyConnection {
 		closeConnection();
 		return listArticle;
 	}
-	
+
+	/**
+	 * List all articles from the database for a given supplier
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	public ArrayList<Article> listAllArticle(Supplier supplier) throws Exception {
+		ArrayList<Article> listArticle = new ArrayList<Article>();
+		Article art = null;
+		try {
+			openConnection();
+
+			String query = "SELECT article.id_article, article.article_quantity, article.article_quantity_real_stock, article.article_state, article.article_creation_date, article.article_update_date, "
+					+ "packaging.packaging_name,product.product_name,article.id_admin FROM article JOIN packaging ON article.id_packaging=packaging.id_packaging JOIN product ON product.id_product=article.id_product "
+					+ "JOIN sell ON sell.id_product = product.id_product JOIN supplier on supplier.id_supplier = sell.id_supplier WHERE (supplier.supplier_name=?);";
+			PreparedStatement declaration = accessDataBase.prepareStatement(query);
+			declaration.setString(1, supplier.getName());
+			ResultSet rs = declaration.executeQuery();
+			/* Récupération des données */
+			while (rs.next()) {
+				Packaging packaging = new Packaging(rs.getString("packaging_name"));
+				Product product = queryProduct.createProductInfo(rs.getString("product_name"));
+				art = new Article(rs.getInt("id_article"), rs.getDouble("article_quantity"),
+						rs.getInt("article_quantity_real_stock"), rs.getString("article_state"),
+						rs.getDate("article_creation_date"), rs.getDate("article_update_date"), packaging, product,
+						rs.getInt("id_admin"));
+				listArticle.add(art);
+			}
+		} catch (Exception e) {
+			System.err.println("Erreur d'affichage d'ing: " + e.getMessage());
+		}
+		closeConnection();
+		return listArticle;
+	}
+
+	/**
+	 * Create object Article with datas from the database
+	 * 
+	 * @param idArticle
+	 * @return
+	 * @throws Exception
+	 */
 	public Article createArticleInfo(int idArticle) throws Exception {
 		openConnection();
 		Article art = null;
@@ -52,8 +102,10 @@ public class QueryArticle extends MyConnection {
 			if (rs.next()) {
 				Packaging packaging = new Packaging(rs.getString("packaging_name"));
 				Product prod = queryProduct.createProductInfo(rs.getString("product_name"));
-				art = new Article(rs.getInt("article.id_article"),rs.getDouble("article_quantity"), rs.getInt("article_quantity_real_stock"),
-						rs.getString("article_state"),rs.getDate("article_creation_date"), rs.getDate("article_update_date"), packaging,prod,rs.getInt("id_admin"));
+				art = new Article(rs.getInt("article.id_article"), rs.getDouble("article_quantity"),
+						rs.getInt("article_quantity_real_stock"), rs.getString("article_state"),
+						rs.getDate("article_creation_date"), rs.getDate("article_update_date"), packaging, prod,
+						rs.getInt("id_admin"));
 			}
 		} catch (Exception e) {
 			System.err.println("Erreur d'affichage d'utilisateur: " + e.getMessage());
@@ -61,5 +113,57 @@ public class QueryArticle extends MyConnection {
 		closeConnection();
 		return art;
 	}
+	
+	public boolean createPrepared(Article article) throws Exception {
+		openConnection();
+		boolean flag = false;
+		try {
+			String query = "INSERT INTO `article`(article_quantity,id_packaging,id_admin,id_product) "
+					+ "VALUES (?,(SELECT id_packaging FROM packaging WHERE packaging_name=?),?,(SELECT id_product FROM product WHERE product_name=?))";
+			PreparedStatement declaration = accessDataBase.prepareStatement(query);
+
+			declaration.setDouble(1, article.getQuantity());
+			declaration.setString(2, article.getPackaging().getName());
+			declaration.setInt(3, article.getIdAdmin());
+			declaration.setString(4, article.getProduct().getName());
+			
+			
+			int executeUpdate = declaration.executeUpdate();
+			flag = (executeUpdate == 1);
+		} catch (Exception e) {
+			System.err.println("Erreur d'insertion utilisateur: " + e.getMessage());
+		}
+		closeConnection();
+		return flag;
+	}
+	
+	public int createPreparedID(Article article) throws Exception {
+		openConnection();
+		int last_inserted_id=0;
+		try {
+			String query = "INSERT INTO `article`(article_quantity,id_packaging,id_admin,id_product) "
+					+ "VALUES (?,(SELECT id_packaging FROM packaging WHERE packaging_name=?),?,(SELECT id_product FROM product WHERE product_name=?))";
+			PreparedStatement declaration = accessDataBase.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
+
+			declaration.setDouble(1, article.getQuantity());
+			declaration.setString(2, article.getPackaging().getName());
+			declaration.setInt(3, article.getIdAdmin());
+			declaration.setString(4, article.getProduct().getName());
+			
+			
+			int executeUpdate = declaration.executeUpdate();
+			ResultSet rs= declaration.getGeneratedKeys();
+			if(rs.next())
+			{
+			last_inserted_id = rs.getInt(1);
+			}
+		} catch (Exception e) {
+			System.err.println("Erreur d'insertion utilisateur: " + e.getMessage());
+		}
+		closeConnection();
+		return last_inserted_id;
+	}
+	
+	
 
 }
