103a16c398da4cdadc48f13257192d96538eb5d7
Tibo30 <71966532+Tibo30@users.noreply.github.com>
2021-02-23 19:52:50 +0100

    add labels

--- ProjetNestiPart2/src/view/Label.java
+++ ProjetNestiPart2/src/view/Label.java
@@ -30,7 +30,7 @@ public class Label extends JLabel {
 				|| (name.equals("Product Price")) || (name.equals("Unit Product"))|| (name.equals("Supplier Product"))
 				|| (name.equals("Article Ref"))|| (name.equals("Article Name"))|| (name.equals("Quantity Article"))
 				|| (name.equals("Unit Article"))|| (name.equals("Packaging Article"))|| (name.equals("Stock Article"))
-				|| (name.equals("Product"))|| (name.equals("Packaging"))|| (name.equals("Quantity Order")) || (name.equals("Quantity"))|| (name.equals("Unit"))|| (name.equals("Product Article"))
+				|| (name.equals("Product"))|| (name.equals("Packaging"))|| (name.equals("Quantity Order")) || (name.equals("Quantity"))|| (name.equals("Unit")) || (name.equals("Date")) || (name.equals("Product Article"))
 				|| (name.equals("Ingredient"))
 				|| (name.equals("Product Unit"))) {
 			this.setFont(new Font("Tahoma", Font.BOLD, 11));
